aaa031ba0fde136c07da69916ee474b54fbf9234
Kartikey Mishr <kartikey.mishra309@gmail.com>
2017-10-25 01:23:01 +0530

    Replaced Dashboard with a new Tabbed One. Added Tabs for Attendance and TimeTable.

--- app/src/main/java/com/kartikeymishr/batch/Dashboard.java
+++ app/src/main/java/com/kartikeymishr/batch/Dashboard.java
@@ -135,7 +135,7 @@ public class Dashboard extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
